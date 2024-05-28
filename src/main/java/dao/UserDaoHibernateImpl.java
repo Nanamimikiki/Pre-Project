5067b0347f04e9182699bf17df27a3824d38925f
Nanamimikiki <nam.prog.acc@gmail.com>
2024-05-28 14:12:18 +0300

    Added base classes

---
+++ src/main/java/dao/UserDaoHibernateImpl.java
@@ -0,0 +1,44 @@
+package dao;
+
+import model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDaoHibernateImpl implements UserDao {
+    public UserDaoHibernateImpl() {
+
+    }
+
+
+    @Override
+    public void createUsersTable() {
+
+    }
+
+    @Override
+    public void dropUsersTable() {
+
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+
+    }
+
+    @Override
+    public void removeUserById(long id) {
+
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<User>();
+        return users;
+    }
+
+    @Override
+    public void cleanUsersTable() {
+
+    }
+}
---
+++ src/main/java/util/Util.java
@@ -0,0 +1,17 @@
+package util;
+
+public class Util {
+//    public static volatile Util instance;
+//
+//    public static Util getInstance() {
+//        Util localInstance = instance;
+//        if (localInstance == null) {
+//            synchronized (Util.class) {
+//                localInstance = instance;
+//                if (localInstance == null) {
+//                    instance = localInstance = new Util();
+//                }
+//            }
+//        }
+//    }
+}
